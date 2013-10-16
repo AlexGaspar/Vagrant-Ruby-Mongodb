cceb08e10c20e1f3a17c90880c4223cc33e4b3a4
Alex <al.gaspar@gmail.com>
2013-10-16 23:06:02 +0200

    Update ruby version

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,16 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
 apt-get update
+apt-get install make
 apt-get install mongodb-10gen -y
 apt-get install nginx -y
-apt-get install ruby -y
 
-
+wget http://cache.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz
+tar -xvzf ruby-2.0.0-p247.tar.gz 
+cd ./ruby-2.0.0-p247
+./configure --prefix=/usr/local
+make
+make install
 # Start Scripts
 service mongodb start
 service nginx start
